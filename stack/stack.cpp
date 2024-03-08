718012cad9db047276b86ff49e6cc48a885fb2fd
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-08 23:40:47 +0800

    Create stack.cpp

---
+++ stack/stack.cpp
@@ -0,0 +1,69 @@
+# include <iostream>
+
+using namespace std;
+
+class Stack{
+private:
+    int *st;
+    int top;
+    int capacity;
+    
+    void double_capacity(){
+        int *tmp = new int [capacity * 2];
+        
+        for(int i = 0; i < capacity; i++) tmp[i] = st[i];
+        delete [] st;
+        st = tmp;
+        capacity *= 2;
+    }
+
+public:
+    Stack(): top(-1), capacity(10){
+        st = new int[capacity];
+    }
+    
+    ~Stack(){
+        delete [] st;
+    }
+    
+    bool empty(){
+        return top == -1;
+    }
+    
+    int size(){
+        return top++;
+    }
+    
+    int get_top(){
+        if(empty()) throw "Stack is empty!";
+        return st[top];
+    }
+    
+    void push(const int val){
+        if(top == capacity - 1) double_capacity();
+        st[++top] = val;
+    }
+    
+    void pop(){
+        if(empty()) throw "Stack is empty!";
+        top--;
+    }
+};
+
+int main(){
+    Stack st;
+    
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    
+    while(!st.empty()){
+        cout << st.get_top() << " ";
+        st.pop();
+    }
+    cout << endl;
+    
+    return 0;
+}
